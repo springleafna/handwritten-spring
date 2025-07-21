e0fe4858ca04197dbb9a5ba2d07babac3903ef41
jwye7 <jwye7@iflytek.com>
2025-07-21 09:20:50 +0800

    feat:创建简单的Bean容器

---
+++ src/test/java/com/springleaf/springframework/ApiTest.java
@@ -0,0 +1,21 @@
+package com.springleaf.springframework;
+
+import com.springleaf.springframework.bean.UserService;
+import org.junit.Test;
+
+public class ApiTest {
+
+    @Test
+    public void test_BeanFactory() {
+        // 1.初始化 BeanFactory
+        BeanFactory beanFactory = new BeanFactory();
+
+        // 2.注册bean对象
+        BeanDefinition beanDefinition = new BeanDefinition(new UserService());
+        beanFactory.registerBeanDefinition("userService", beanDefinition);
+
+        // 3.获取bean
+        UserService userService = (UserService) beanFactory.getBean("userService");
+        userService.queryUserInfo();
+    }
+}
---
+++ src/test/java/com/springleaf/springframework/bean/UserService.java
@@ -0,0 +1,9 @@
+package com.springleaf.springframework.bean;
+
+public class UserService {
+
+    public void queryUserInfo(){
+        System.out.println("查询用户信息");
+    }
+
+}
